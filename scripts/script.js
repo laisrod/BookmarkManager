30fc7ca4d09549a322bc057ba6e0128bd6d45cb0
Laís Rodrigues <devlaisrodrigues@gmail.com>
2025-07-30 05:53:17 -0300

    restructuring folder2

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,10 @@
-const menuToggle = document.querySelector('.menu-toggle'); // procure no site o elemento com a classe menu-toggle, encontra o botao hamburguer
-const menuList = document.querySelector('.header__list'); // procure no site o elemento com a classe header__list, Encontra a lista de links do menu
-const menuClose = document.querySelector('.menu-close'); // procure no site o elemento com a classe menu-close, Encontra o botão X para fechar
-
+const menuToggle = document.querySelector('.menu-toggle'); 
+const menuList = document.querySelector('.header__list'); 
+const menuClose = document.querySelector('.menu-close');
 const section = document.querySelector('.section');
 
-console.log('SECTION', section); //mostra a seção no console, É como um teste para ver se o JavaScript encontrou os elementos corretos.
-
-// Função do Menu Mobile
-menuToggle.addEventListener('click', function() { // Quando alguém clicar no botão hambúrguer, execute esta função"
+console.log('SECTION', section); 
+menuToggle.addEventListener('click', function() {
   menuList.classList.toggle('open');
   menuToggle.classList.add('hide');
   menuClose.classList.add('show');
@@ -15,26 +12,20 @@ menuToggle.addEventListener('click', function() { // Quando alguém clicar no bo
   menuToggle.setAttribute('aria-expanded', !expanded);
 });
 
-//hamburguer
 menuClose.addEventListener('click', function() {
-  menuList.classList.remove('open'); //mostra o menu
-  menuToggle.classList.remove('hide'); //esconde o hamburguer
-  menuClose.classList.remove('show'); //mostra o x
+  menuList.classList.remove('open');
+  menuToggle.classList.remove('hide');
+  menuClose.classList.remove('show');
   menuToggle.setAttribute('aria-expanded', false);
 });
 
-// para tabs
+
 document.querySelectorAll('.features-container-list a').forEach(link => {
   link.addEventListener('click', function(e) {
-    //remove destaque de todas as abas
       document.querySelectorAll('.features-container-list li').forEach(li => li.classList.remove('active'));
-      //destaca a aba clicada
       this.parentElement.classList.add('active');
-      //mostra o conteudo da aba clicada
       const tab = this.getAttribute('data-tab');
-      //esconde todos os conteudos
       document.querySelectorAll('.tab-content').forEach(tab => tab.style.display = 'none');
-      //mostra o conteudo da aba clicada
       document.querySelector(`#${tab}`).style.display = 'block';
   });
 
